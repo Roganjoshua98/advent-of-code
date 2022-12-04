ca7be051cd55547c5ea04ada9ad058c064d79e9d
Roganjoshua98 <35848559+Roganjoshua98@users.noreply.github.com>
2022-12-04 16:28:41 +0000

    day 4 part two complete

--- 2022/day04/main.go
+++ 2022/day04/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	file, _ := os.Open("input.txt")
-	x := partOne(file)
-	fmt.Println(x)
+	fmt.Println(partOne(file))
+	fmt.Println(partTwo(file))
 }
 
 func partOne(file *os.File) int {
@@ -23,6 +23,23 @@ func partOne(file *os.File) int {
 			(pairs[0][0] >= pairs[1][0] && pairs[0][1] <= pairs[1][1]) {
 				total += 1
 			}
+	}
+	return total
+}
+
+func partTwo(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+	total := 0
+	for scanner.Scan() {
+		pairs := lineToPairs(scanner.Text())
+		// Easier to prove if they *don't* overlap
+		min, max := pairs[0], pairs[1]
+		if pairs[1][0] < pairs[0][1] {
+			min, max = pairs[1], pairs[0]
+		}
+		if !(min[1] < max[0]) {
+			total += 1
+		}
 	}
 	return total
 }
